4a489bff9a2b47a570ea7d7578c4cf5b66b73870
Daniel Kluver <kluver@cs.umn.edu>
2014-01-14 17:10:59 -0600

    pretty well tuned baseline algorithms and item-item (all things considered)

--- lenskit/src/eval/chart.R
+++ lenskit/src/eval/chart.R
@@ -1,25 +1,43 @@
 # Create a chart comparing the algorithms
 
+#cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
 library("ggplot2")
 
 all.data <- read.csv("eval-user.csv")
 
 serr = function(d) {qnorm(0.975)*sd(d)/sqrt(length(d))}
 
-all.mean <- aggregate(cbind(RMSE, nDCG) ~ Algorithm+DataSet, data=all.data, mean)
-all.serr <- aggregate(cbind(RMSE, nDCG) ~ Algorithm+DataSet, data=all.data, serr)
-names(all.serr) <- c("Algorithm", "DataSet", "RMSEERR", "nDCGERR")
-all.agg = merge(all.serr, all.mean)
+all.mean <- aggregate(cbind(RMSE, MAE, nDCG, Information.ByUser) ~ Algorithm+DataSet, data=all.data, mean)
+all.serr <- aggregate(cbind(RMSE, MAE, nDCG, Information.ByUser) ~ Algorithm+DataSet, data=all.data, serr)
+names(all.serr) <- c("Algorithm", "DataSet", "RMSEE", "MAEE", "nDCGE", "Information.ByUserE")
+all.agg = merge(all.mean, all.serr)
 
-rmse = ggplot(all.agg,aes(x=DataSet, y=RMSE, ymin=RMSE-RMSEERR, ymax=RMSE+RMSEERR, color=Algorithm))+geom_line()+geom_errorbar()
-ndcg = ggplot(all.agg,aes(x=DataSet, y=nDCG, ymin=nDCG-nDCGERR, ymax=nDCG+nDCGERR, color=Algorithm))+geom_line()+geom_errorbar()
+rmse = ggplot(all.agg,aes(x=DataSet, y=RMSE, ymin=RMSE-RMSEE, ymax=RMSE+RMSEE, color=Algorithm))+geom_line()+geom_errorbar()
 
+rmse
 print("Outputting to rmse.pdf")
 pdf("rmse.pdf", paper="letter", width=0, height=0)
 print(rmse)
 dev.off()
 
+mae = ggplot(all.agg,aes(x=DataSet, y=MAE, ymin = MAE-MAEE, ymax=MAE+MAEE, color=Algorithm))+geom_line()+geom_errorbar()
+
+print("Outputting to mae.pdf")
+pdf("mae.pdf", paper="letter", width=0, height=0)
+print(mae)
+dev.off()
+
+ndcg = ggplot(all.agg,aes(x=DataSet, y=nDCG, ymin=nDCG-nDCGE, ymax=nDCG+nDCGE, color=Algorithm))+geom_line()+geom_errorbar()
+
 print("Outputting to ndcg.pdf")
 pdf("ndcg.pdf", paper="letter", width=0, height=0)
 print(ndcg)
+dev.off()
+
+info = ggplot(all.agg,aes(x=DataSet, y=Information.ByUser, ymin=Information.ByUser-Information.ByUserE, ymax =Information.ByUser+Information.ByUserE, color=Algorithm))+geom_line()+geom_errorbar()
+
+print("Outputting to info.pdf")
+pdf("info.pdf", paper="letter", width=0, height=0)
+print(info)
 dev.off()
--- lenskit/src/eval/eval.groovy
+++ lenskit/src/eval/eval.groovy
@@ -16,13 +16,44 @@ import org.grouplens.lenskit.baseline.*
 import org.grouplens.lenskit.transform.normalize.*
 
 import org.apache.commons.lang3.BooleanUtils
+import org.grouplens.lenskit.vectors.similarity.CosineVectorSimilarity
+import org.grouplens.lenskit.vectors.similarity.VectorSimilarity
 
-def zipFile = "${config.dataDir}/ml-1m.zip"
-def dataDir = config.get('mldata.directory', "${config.dataDir}/ml-1m")
-def MAX=20
+def MAX=19
 
 // This target unpacks the data
-sourceDataset = target('download') {
+sourceDataset100k = target('download100k') {
+    
+    def zipFile = "${config.dataDir}/ml-100k.zip"
+    def dataDir = config.get('mldata.directory', "${config.dataDir}/ml-100k")
+
+    ant.mkdir(dir: config.dataDir)
+    ant.get(src: 'http://www.grouplens.org/system/files/ml-100k.zip',
+            dest: zipFile,
+            skipExisting: true)
+    ant.unzip(src: zipFile, dest: dataDir) {
+        patternset {
+            include name: 'ml-100k/*'
+        }
+        mapper type: 'flatten'
+    }
+    perform {
+        return csvfile("${dataDir}/u.data") {
+            delimiter "\t"
+            domain {
+                minimum 1
+                maximum 5
+                precision 1.0
+            }
+        }
+    }
+}
+
+// This target unpacks the data
+sourceDataset1m = target('download1m') {
+    def zipFile = "${config.dataDir}/ml-1m.zip"
+    def dataDir = config.get('mldata.directory', "${config.dataDir}/ml-1m")
+
     ant.mkdir(dir: config.dataDir)
     ant.get(src: 'http://files.grouplens.org/datasets/movielens/ml-1m.zip',
             dest: zipFile,
@@ -33,22 +64,25 @@ sourceDataset = target('download') {
         }
         mapper type: 'flatten'
     }
-    csvfile("${dataDir}/ratings.dat") {
-        delimiter "::"
-        domain {
-            minimum 1
-            maximum 5
-            precision 0.5
+    perform {
+        csvfile("${dataDir}/ratings.dat") {
+            delimiter "::"
+            domain {
+                minimum 1
+                maximum 5
+                precision 0.5
+            }
         }
     }
 }
 
-data = []
+def sourceDataset = sourceDataset1m
 
 def datasets = target('do-crossfolds') {
     requires sourceDataset
     
-    for (i in 0..MAX) {
+    def data = []
+    for (i in (0 .. 9).collect{it*2}) {
         d = crossfold (""+i) {
             source sourceDataset
             test "${config.dataDir}/ml-1m-crossfold/test"+i+".%d.csv"
@@ -59,79 +93,23 @@ def datasets = target('do-crossfolds') {
         }
         data +=d
     }
-    data
-}
-
-// Let's define some algorithms
-def itemitem = algorithm("ItemItem") {
-    // use the item-item rating predictor with a baseline and normalizer
-    bind ItemScorer to ItemItemScorer
-    bind (BaselineScorer, ItemScorer) to UserMeanItemScorer
-    bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
-    bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
-    
-    // retain 500 neighbors in the model, use 30 for prediction
-    set ModelSize to 250
-    set NeighborhoodSize to 50
-
-    // apply some Bayesian smoothing to the mean values
-    within(BaselineScorer, ItemScorer) {
-        set MeanDamping to 25.0d
+    perform {
+        result = []
+        for (d in data) {
+            result += d.get()
+        }
+        return result
     }
 }
-
-def useruser = algorithm("UserUser") {
-    // use the user-user rating predictor
-    bind ItemScorer to UserUserItemScorer
-    bind (BaselineScorer, ItemScorer) to UserMeanItemScorer
-    bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
-    bind VectorNormalizer to MeanVarianceNormalizer
-
-    // use 30 neighbors for predictions
-    set NeighborhoodSize to 30
-
-    // override normalizer within the neighborhood finder
-    // this makes it use a different normalizer (subtract user mean) for computing
-    // user similarities
-    within(NeighborhoodFinder) {
-        bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
-        // override baseline to use user mean
-        bind (UserMeanBaseline, ItemScorer) to GlobalMeanRatingItemScorer
-    }
-
-    // and apply some Bayesian damping to the baseline
-    within(BaselineScorer, ItemScorer) {
-        set MeanDamping to 25.0d
-    }
-}
-/*
-def ds = csvfile("${dataDir}/ratings.dat") {
-    delimiter "::"
-    domain {
-        minimum 1
-        maximum 5
-        precision 0.5
-    }
-    cache true
-}
-
-def oracle = algorithm("Oracle") {
-    bind RatingPredictor to OracleRatingPredictor
-    within (OracleRatingPredictor) {
-        bind DataSource to ds
-        bind EventDAO to ds.getEventDAO()
-    }
-}*/
 
 target('evaluate') {
     // this requires the ml100k target to be run first
     // can either reference a target by object or by name (as above)
-    requires "do-crossfolds"
+    requires datasets
 
     trainTest {
-        for (d in data) {
-            dataset d
-        }
+        dataset datasets
+        
         
         // Three different types of output for analysis.
         output "${config.analysisDir}/eval-results.csv"
@@ -143,10 +121,54 @@ target('evaluate') {
         metric MAEPredictMetric
         metric NDCGPredictMetric
         metric EntropyPredictMetric
+
+        algorithm("UserItemBaseline5") {
+            bind ItemScorer to UserMeanItemScorer
+            bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
+            set MeanDamping to 5.0d
+        }
         
-        algorithm itemitem
+        algorithm("ItemBaseline5") {
+            bind ItemScorer to ItemMeanRatingItemScorer
+            set MeanDamping to 5.0d
+        }
+
+        algorithm("ItemItemItemBaseLine") {
+            // use the item-item rating predictor with a baseline and normalizer
+            bind ItemScorer to ItemItemScorer
+            bind (BaselineScorer, ItemScorer) to ItemMeanRatingItemScorer
+            bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
+            bind VectorSimilarity to CosineVectorSimilarity
+
+            // retain 500 neighbors in the model, use 30 for prediction
+            set ModelSize to 250
+            set NeighborhoodSize to 50
+
+            // apply some Bayesian smoothing to the mean values
+            within(BaselineScorer, ItemScorer) {
+                set MeanDamping to 5.0d
+            }
+        }
+
+        algorithm("ItemItemNoBaseLine") {
+            // use the item-item rating predictor with a baseline and normalizer
+            bind ItemScorer to ItemItemScorer
+            bind VectorSimilarity to CosineVectorSimilarity
+            //bind (BaselineScorer, ItemScorer) to UserMeanItemScorer
+            //bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
+            //bind UserVectorNormalizer to BaselineSubtractingUserVectorNormalizer
+
+            // retain 500 neighbors in the model, use 30 for prediction
+            set ModelSize to 250
+            set NeighborhoodSize to 50
+
+            // apply some Bayesian smoothing to the mean values
+            //within(BaselineScorer, ItemScorer) {
+            //    set MeanDamping to 25.0d
+            //}
+        }
         //algorithm oracle
-        algorithm useruser
+        //algorithm useruser
     }
 }
 
