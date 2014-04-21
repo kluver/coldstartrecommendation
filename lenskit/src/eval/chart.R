2cac0e2d3979e78b1d67530a25f5f4452c176139
Daniel Kluver <kluver@cs.umn.edu>
2014-04-21 17:31:07 -0500

    some work

--- lenskit/src/eval/chart.R
+++ lenskit/src/eval/chart.R
@@ -1,27 +1,26 @@
 # Create a chart comparing the algorithms
 
-cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 library("ggplot2")
 
 all.data <- read.csv("eval-user.csv")
 
-serr = function(d) {qnorm(0.975)*sd(d)/sqrt(length(d))}
 nonMetrics = c("Algorithm","Retain", "DataSet", "User", "Partition", "NGood", "TestEvents", "TrainEvents", "NAttempted", "RatingEntropy")
 
 metrics = setdiff(names(all.data),nonMetrics)
 
 for (metric in metrics) {
-  plot = ggplot(all.data, aes_string(x="Retain", y=metric, color="Algorithm"))+stat_summary(fun.y=mean, geom="line") + stat_summary(fun.data = mean_cl_normal, geom="errorbar")
+  plot = ggplot(all.data, aes_string(x="Retain", y=metric, color="Algorithm", shape="Algorithm"))+stat_summary(fun.y=mean, geom="line")+stat_summary(fun.y=mean, geom="point") + stat_summary(fun.data = mean_cl_normal, geom="errorbar")
+  metric = gsub('\\.', '_', metric)
   filename = paste(metric,".pdf",sep="")
   cat("Outputting to",filename,"\n")
-  pdf(filename, width=11, height=8.5)
+  pdf(filename, width=5.5, height=4.25)
   print(plot)
   dev.off()
 }
 
 
 dat <- read.csv("eval-results.csv")
-plot = ggplot(dat, aes(x=Retain, y=TopN.Entropy, color=Algorithm))+geom_point()+stat_summary(fun.y=mean, geom="line")
-pdf("topN.entropy.pdf", width=11, height=8.5)
+plot = ggplot(dat, aes(x=Retain, y=TopN.Entropy, color=Algorithm, shape=Algorithm))+geom_point()+stat_summary(fun.y=mean, geom="line")
+pdf("topN_entropy.pdf", width=5.5, height=4.5)
 print(plot)
 dev.off()
