e691d74b46ac4314e817cfef92050eda54ab5919
Daniel Kluver <kluver@cs.umn.edu>
2014-05-04 19:52:35 -0500

    prepped for re-re-re-run

--- lenskit/src/eval/eval.groovy
+++ lenskit/src/eval/eval.groovy
@@ -47,8 +47,8 @@ import org.hamcrest.collection.IsIn
 //def sizes = [0,1,2,3]
 //def sizes = [0,8,16,32,128]
 //def sizes = [0,1,2,4,8,12,16,19]
-//def sizes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-def sizes = [0,2,4,8,19]
+def sizes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+//def sizes = [0,2,4,8,19]
 //def sizes = [0,2,4,8]
 //def sizes = [0,3,9,18]
 //def sizes = [0,1,2,4,8]
@@ -331,7 +331,7 @@ target('evaluate') {
             include diversityConfig
         }/**/
 
-        /*algorithm("UserUser") {
+        algorithm("UserUser") {
             bind ItemScorer to UserUserItemScorer
             bind VectorSimilarity to CosineVectorSimilarity
             bind (BaselineScorer, ItemScorer) to UserMeanItemScorer
@@ -344,7 +344,7 @@ target('evaluate') {
             include diversityConfig
         }/**/
         
-        /*algorithm("svd") {
+        algorithm("svd") {
             bind ItemScorer to FunkSVDItemScorer
             bind (BaselineScorer, ItemScorer) to UserMeanItemScorer
             bind (UserMeanBaseline, ItemScorer) to ItemMeanRatingItemScorer
