51f5a1f820bf3abed1669741f38c7ba50688db53
Daniel Kluver <kluver@cs.umn.edu>
2014-05-07 18:36:22 -0500

    oops, forgot to save a file

--- lenskit/src/eval/chart.R
+++ lenskit/src/eval/chart.R
@@ -15,22 +15,21 @@ data.melt = aggregate(value~Algorithm+Retain+variable, data=all.data.melt, mean)
 
 # redo some names
 data.melt$variable = as.character(data.melt$variable)
-data.melt$variable[data.melt$variable == "MAP"] = "MAP@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "Precision"] = "Precision@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "Precision.fallout"] = "Fallout@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "TopN.RMSE.seenItems"] = "SeenItems@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "TopN.RMSE"] = "RMSE@20"
-data.melt$variable[data.melt$variable == "TopN.Ave.Rat"] = "MeanRating@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "TopN.MeanPopularity"] = "AveragePopularity@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "diversity"] = "AILS@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-data.melt$variable[data.melt$variable == "nDCG"] = "nDCG\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted"
-
-data.melt$variable = ordered(data.melt$variable, levels = c("Coverage", "RMSE", "nDCG\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "Precision@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "MAP@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "Fallout@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "SeenItems@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "MeanRating@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "RMSE@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "AveragePopularity@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "AILS@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "TestTime","MAE","MutualInformation","PredicitonEntropy","ItemScorer.NAttempted","ItemScorer.NGood","ItemScorer.Coverage","TopN.ActualLength", "Recall.fallout", "Recall"))
+data.melt$variable[data.melt$variable == "MAP"] = "MAP@20 ³"
+data.melt$variable[data.melt$variable == "Precision"] = "Precision@20 ³"
+data.melt$variable[data.melt$variable == "Precision.fallout"] = "Fallout@20 ³"
+data.melt$variable[data.melt$variable == "TopN.RMSE.seenItems"] = "SeenItems@20 ³"
+data.melt$variable[data.melt$variable == "TopN.RMSE"] = "RMSE@20 ⁴"
+data.melt$variable[data.melt$variable == "TopN.Ave.Rat"] = "MeanRating@20 ³⁴"
+data.melt$variable[data.melt$variable == "TopN.MeanPopularity"] = "AveragePopularity@20 ³"
+data.melt$variable[data.melt$variable == "diversity"] = "AILS@20 ³"
+data.melt$variable[data.melt$variable == "nDCG"] = "nDCG ³"
+data.melt$variable = ordered(data.melt$variable, levels = c("Coverage", "RMSE", "nDCG ³", "Precision@20 ³", "MAP@20 ³", "Fallout@20 ³", "SeenItems@20 ³", "MeanRating@20 ³⁴", "RMSE@20 ⁴", "AveragePopularity@20 ³", "AILS@20 ³", "TestTime","MAE","MutualInformation","PredicitonEntropy","ItemScorer.NAttempted","ItemScorer.NGood","ItemScorer.Coverage","TopN.ActualLength", "Recall.fallout", "Recall"))
 
 data.melt$Algorithm = ordered(data.melt$Algorithm, c("ItemItem", "UserUser", "svd", "ItemBaseline", "UserItemBaseline"))
 
 # make plots
-vars = c("RMSE", "nDCG\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted")
+vars = c("RMSE", "nDCG ³")
 data.melt.sub = subset(data.melt, variable %in% vars)
 data.melt.sub = subset(data.melt.sub, Algorithm != "UserItemBaseline" | variable == "RMSE")
 plot = ggplot(data.melt.sub, aes(x=Retain, y=value, color=Algorithm, shape=Algorithm))+geom_point()+geom_line()+scale_x_continuous(breaks=4*(0:5))+facet_wrap(~variable, scales="free_y", nrow=1)+scale_color_manual(values=cp)+theme_minimal()+xlab("Simulated Profile Size")+theme(legend.position="bottom") + theme(plot.margin = unit(c(0,0,0,0), "cm"))
@@ -39,7 +38,7 @@ print(plot)
 dev.off()
 
 
-vars = c("Precision@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "MAP@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "Fallout@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted")
+vars = c("Precision@20 ³", "MAP@20 ³", "Fallout@20 ³")
 data.melt.sub = subset(data.melt, variable %in% vars)
 data.melt.sub = subset(data.melt.sub, Algorithm != "UserItemBaseline")
 plot = ggplot(data.melt.sub, aes(x=Retain, y=value, color=Algorithm, shape=Algorithm))+geom_point()+geom_line()+scale_x_continuous(breaks=4*(0:5))+facet_wrap(~variable, scales="free_y", nrow=1)+scale_color_manual(values=cp)+theme_minimal()+xlab("Simulated Profile Size")+theme(legend.position="bottom") + theme(plot.margin = unit(c(0,0,0,0), "cm"))
@@ -47,13 +46,13 @@ pdf("TopNPrecision.pdf", width=8.5, height=3.2)
 print(plot)
 dev.off()
 
-vars = c("SeenItems@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "MeanRating@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "RMSE@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted")
+vars = c("SeenItems@20 ³", "MeanRating@20 ³⁴", "RMSE@20 ⁴")
 data.melt.sub = subset(data.melt, variable %in% vars)
-data.melt.sub = subset(data.melt.sub, Algorithm != "UserUser" | variable == "SeenItems@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted")
-data.melt.sub = subset(data.melt.sub, Algorithm != "UserItemBaseline" | variable == "RMSE@20")
+data.melt.sub = subset(data.melt.sub, Algorithm != "UserUser" | variable == "SeenItems@20 ³")
+data.melt.sub = subset(data.melt.sub, Algorithm != "UserItemBaseline" | variable == "RMSE@20 ⁴")
 
 plot = ggplot(data.melt.sub, aes(x=Retain, y=value, color=Algorithm, shape=Algorithm))+geom_point()+geom_line()+scale_x_continuous(breaks=4*(0:5))+facet_wrap(~variable, scales="free_y", nrow=1)+scale_color_manual(values=cp)+theme_minimal()+xlab("Simulated Profile Size")+theme(legend.position="bottom") + theme(plot.margin = unit(c(0,0,0,0), "cm"))
-pdf("rmse_20.pdf", width=8.5, height=3.2)
+cairo_pdf("rmse_20.pdf", width=8.5, height=3.2)
 print(plot)
 dev.off()
 
@@ -61,12 +60,12 @@ spread.dat <- read.csv("eval-results.csv")
 spread.dat = spread.dat[c("Algorithm","Retain", "TopN.Entropy", "Partition")]
 spread.mean = aggregate(TopN.Entropy~Algorithm+Retain, data=spread.dat, mean)
 names(spread.mean) <- c("Algorithm", "Retain", "value")
-spread.mean$variable = "Spread@20"
+spread.mean$variable = "Spread@20 ³"
 
-vars = c("AveragePopularity@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "AILS@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted")
+vars = c("AveragePopularity@20 ³", "AILS@20 ³")
 data.melt.sub = subset(data.melt, variable %in% vars)
 data.melt.sub = rbind(data.melt.sub, spread.mean)
-data.melt.sub$variable = ordered(data.melt.sub$variable, levels = c("AveragePopularity@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "AILS@20\nUserItemBaseline is equivalent to ItemBaseline on this metric, and is not plotted", "Spread@20"))
+data.melt.sub$variable = ordered(data.melt.sub$variable, levels = c("AveragePopularity@20 ³", "AILS@20 ³", "Spread@20 ³"))
 
 data.melt.sub = subset(data.melt.sub, Algorithm != "UserItemBaseline")
 plot = ggplot(data.melt.sub, aes(x=Retain, y=value, color=Algorithm, shape=Algorithm))+geom_point()+geom_line()+scale_x_continuous(breaks=4*(0:5))+facet_wrap(~variable, scales="free_y", nrow=1)+scale_color_manual(values=cp)+theme_minimal()+xlab("Simulated Profile Size")+theme(legend.position="bottom") + theme(plot.margin = unit(c(0,0,0,0), "cm"))
